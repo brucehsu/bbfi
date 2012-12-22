6ac2661a3b4b536f768e489acfcbdde22e7334b7
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2012-12-22 17:36:21 +0800

    Initial execution structure.

--- bbfi.c
+++ bbfi.c
@@ -1,5 +1,28 @@
 #include "bbfi.h"
 
 int main(int argc, char **argv) {
-	/* return interpret(); */
+	return interpret(); 
+}
+
+int interpret() {
+	char c;
+	while((c=fgetc(stdin))) {
+		switch(c) {
+			case '>':
+				break;
+			case '<':
+				break;
+			case '+':
+				break;
+			case '-':
+				break;
+			case '.':
+				break;
+			case ',':
+				break;
+			case '[':
+				break;
+		}
+	}
+	return 0;
 }
