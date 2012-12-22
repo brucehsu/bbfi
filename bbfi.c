5fb7b8cf02b86a9bcc5ecff412b7ff071d89d1f7
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2012-12-22 20:46:24 +0800

    Operation ><+-., implemented

--- bbfi.c
+++ bbfi.c
@@ -6,23 +6,90 @@ int main(int argc, char **argv) {
 
 int interpret() {
 	char c;
-	while((c=fgetc(stdin))) {
+	Heap* heap = createHeap();
+	while((c=fgetc(stdin))!=EOF) {
 		switch(c) {
 			case '>':
+				heap = movePtrToNext(heap);
 				break;
 			case '<':
+				heap = movePtrToPrev(heap);
+				if(heap==0) return 13;
 				break;
 			case '+':
+				heap = addPtr(heap);
 				break;
 			case '-':
+				heap = subPtr(heap);
 				break;
 			case '.':
+				heap = printPtr(heap);
 				break;
 			case ',':
+				heap = inputPtr(heap);
 				break;
 			case '[':
 				break;
 		}
 	}
 	return 0;
+}
+
+Heap* createHeap() {
+	Heap* heap = (Heap*) malloc(sizeof(Heap));
+	memset(heap,0,sizeof(Heap));
+	heap->ptr = heap->obj;
+	return heap;
+}
+
+Heap* expandHeap(Heap *currentHeap) {
+	Heap* heap = createHeap();
+	currentHeap->next = heap;
+	heap->prev = currentHeap;
+	return heap;
+}
+
+Heap* movePtrToNext(Heap* heap) {
+	if(heap->currentIndex<HEAP_MAX-1) {
+		++(heap->ptr);
+		++(heap->currentIndex);
+	} else {
+		if(heap->next) {
+			return heap->next;
+		} else {
+			heap = expandHeap(heap);
+		}
+	}
+	return heap;
+}
+
+Heap* movePtrToPrev(Heap* heap) {
+	if(heap->currentIndex==0) {
+		if(heap->prev==0) return 0;
+		heap = heap->prev;
+	} else {
+		--(heap->ptr);
+		--(heap->currentIndex);
+	}
+	return heap;
+}
+
+Heap* addPtr(Heap* heap) {
+	++(*(heap->ptr));
+	return heap;
+}
+
+Heap* subPtr(Heap* heap) {
+	--(*(heap->ptr));
+	return heap;	
+}
+
+Heap* printPtr(Heap* heap) {
+	fputc((int) *(heap->ptr), stdout);
+	return heap;
+}
+
+Heap* inputPtr(Heap* heap) {
+	*(heap->ptr) = fgetc(stdin);
+	return heap;
 }
--- bbfi.h
+++ bbfi.h
@@ -1,20 +1,22 @@
 #define HEAP_MAX 10000
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct heap{
+typedef struct Heap{
 	char *ptr;
+	int currentIndex;
 	char obj[HEAP_MAX];
-	struct heap *prev, *next;
-} heap;
+	struct Heap *prev, *next;
+} Heap;
 
 int main(int argc, char **argv);
 int interpret();
-heap* expandHeap(heap* currentHeap);
-heap* createHeap();
-char* movePtrToNext(heap* heap);
-char* movePtrToPrev(heap* heap);
-char* addPtr(heap* heap);
-char* subPtr(heap* heap);
-char* printPtr(heap* heap);
-char* inputPtr(heap* heap);
+Heap* expandHeap(Heap* currentHeap);
+Heap* createHeap();
+Heap* movePtrToNext(Heap* heap);
+Heap* movePtrToPrev(Heap* heap);
+Heap* addPtr(Heap* heap);
+Heap* subPtr(Heap* heap);
+Heap* printPtr(Heap* heap);
+Heap* inputPtr(Heap* heap);
